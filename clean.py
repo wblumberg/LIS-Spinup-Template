f7268cbfd8c0b1fc020fff3e26fbaae526fbb97c
Greg Blumberg <wblumberg@ou.edu>
2020-09-17 16:48:59 -0400

    Updated common.reg with correct libaries

--- clean.py
+++ clean.py
@@ -8,3 +8,15 @@ for f in new_files:
 
 print("Cleaned " + str(len(new_files)) + ' files.')
 
+tbl = glob.glob("*.TBL")
+for t in tbl:
+    print('rm ' + t)
+    os.system('rm ' + t)
+
+print("Cleaned " + str(len(tbl)) + ' files.')
+
+print("Cleaning LDT/LIS config files.")
+for t in ['ldt.config.postlis', 'ldt.config.prelis', 'lis.config.coldstart', 'lis.config.wrf']:
+    os.system('rm ' + t)
+    os.system('rm ' + t + '.new')
+
